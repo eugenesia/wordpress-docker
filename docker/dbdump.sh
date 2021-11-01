c2d5ba7083847ff64fdeb7071f2af3033658a4b9
Eugene Sia <eugene@eugenesia.co.uk>
2021-11-01 23:44:53 +0000

    Add date to filename

--- docker/dbdump.sh
+++ docker/dbdump.sh
@@ -5,11 +5,15 @@ set -o allexport
 source .env
 set +o allexport
 
+dateStr=$(date +"%Y%m%d_%H%M")
+
 mysqldump \
   --host 127.0.0.1 \
   --port $MYSQL_FORWARD_PORT \
   --user $MYSQL_USER \
-  --password $MYSQL_PASSWORD \
+  --password="$MYSQL_PASSWORD" \
+  --no-tablespaces \
+  --column-statistics=0 \
   -- $MYSQL_DATABASE \
-  | gzip > wordpress.sql.gz
+  | gzip > wordpress_$dateStr.sql.gz
 
